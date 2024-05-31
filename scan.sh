a8b876cd82c9734e4eeca7ae21c664253990b36f
expo <mathis.baltisbe@hes-so.ch>
2024-05-31 15:36:41 +0200

    scan shell update

--- scan.sh
+++ scan.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 
-scanimage --format=png --output-file scans/tmp/scan.png
+scanimage --format=png --output-file /home/frijol/Desktop/no_recollection/scans/tmp/scan.png
 sleep 3
 
-find scans/tmp/ -type f -empty -print0 | xargs -0 rm
+find /home/frijol/Desktop/no_recollection/scans/tmp/ -type f -empty -print0 | xargs -0 rm
 
-if test -e scans/tmp/scan.png; then
-    echo "File exists"
-    ./multicrop -c 20,20 -f 20 -d 100 scans/tmp/scan.png scans/tmp/scan_trimmed.png
-     convert scans/tmp/scan_trimmed-000.png -crop 1100x+0+0 scans/postcard-$(date "+%FT%H%M%S").png
+if test -e /home/frijol/Desktop/no_recollection/scans/tmp/scan.png; then
+    file_created=1
+    cp /home/frijol/Desktop/no_recollection/scans/tmp/scan.png /home/frijol/Desktop/no_recollection/scans/archive/postcard-$(date "+%FT%H%M%S").png
 else
-    echo "File does not exist"
+    file_created=0
 fi
+
+echo $file_created
